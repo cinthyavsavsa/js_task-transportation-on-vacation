cb81310ca575319c2f9845ee3d528e2de4184b3f
Cinthya <cinthyavaleskaa@gmail.com>
2026-09-17 19:41:54 -0300

    Refactor: use constants and prefer const over let

--- src/calculateRentalCost.js
+++ src/calculateRentalCost.js
@@ -1,18 +1,26 @@
+const DAILY_RATE = 40;
+const LONG_TERM_DAYS = 7;
+const LONG_TERM_DISCOUNT = 50;
+const SHORT_TERM_DAYS = 3;
+const SHORT_TERM_DISCOUNT = 20;
+
 /**
  * @param {number} days
  *
  * @return {number}
  */
 function calculateRentalCost(days) {
-  let price = days * 40;
+  const basePrice = days * DAILY_RATE;
 
-  if (days >= 7) {
-    price -= 50;
-  } else if (days >= 3) {
-    price -= 20;
+  if (days >= LONG_TERM_DAYS) {
+    return basePrice - LONG_TERM_DISCOUNT;
   }
 
-  return price;
+  if (days >= SHORT_TERM_DAYS) {
+    return basePrice - SHORT_TERM_DISCOUNT;
+  }
+
+  return basePrice;
 }
 
 module.exports = calculateRentalCost;
